226ebc97f6d1bc366b2a1c899c8ef386d5acedfb
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-05-02 14:09:20 +0200

    Adding code examples for rule 1

---
+++ src/Rule1/BadExample/ProcessOrder.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gsoares\ObjectCalisthenics\Rule1\BadExample;
+
+use Exception;
+
+class ProcessOrder
+{
+    /** @var object */
+    private $orderRepository;
+
+    /** @var object */
+    private $stockService;
+
+    /** @var object */
+    private $shippingService;
+
+    public function process(array $orderIds): void
+    {
+        foreach ($orderIds as $orderId) {
+            $order = $this->orderRepository->find($orderId);
+
+            if ($order) {
+                foreach ($order->getItems() as $item) {
+                    if ($this->stockService->hasStock($item)) {
+                        $this->stockService->decreaseStock($item);
+                    } else {
+                        throw new Exception('Insufficient Stock');
+                    }
+                }
+            } else {
+                throw new Exception('Order does not exist');
+            }
+
+            $this->shippingService->ship($order);
+        }
+    }
+}
---
+++ src/Rule1/GoodExample/ProcessOrder.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gsoares\ObjectCalisthenics\Rule1\GoodExample;
+
+use Exception;
+
+class ProcessOrder
+{
+    /** @var object */
+    private $orderRepository;
+
+    /** @var object */
+    private $stockService;
+
+    /** @var object */
+    private $shippingService;
+
+    public function process(array $orderIds): void
+    {
+        foreach ($orderIds as $orderId) {
+            $this->processOrder($orderId);
+        }
+    }
+
+    private function processOrder(int $orderId)
+    {
+        $order = $this->getOrderById($orderId);
+
+        foreach ($order->getItems() as $item) {
+            $this->processItem($item);
+        }
+
+        $this->shippingService->ship($order);
+    }
+
+    private function processItem(object $item): void
+    {
+        if ($this->stockService->hasStock($item)) {
+            throw new Exception('Insufficient Stock');
+        }
+
+        $this->stockService->decreaseStock($item);
+    }
+
+    private function getOrderById(int $orderId): object
+    {
+        $order = $this->orderRepository->find($orderId);
+
+        if ($order) {
+            return $order;
+        }
+
+        throw new Exception('Order does not exist');
+    }
+}
